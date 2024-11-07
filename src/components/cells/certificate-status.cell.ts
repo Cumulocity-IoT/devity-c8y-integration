4f81775f6274367a58ef139453b70df0ae510ebb
Dirk Peter <dirk.peter@softwareg.com>
2024-11-07 14:33:53 +0100

    prepares revoke and renew

--- src/components/cells/certificate-status.cell.ts
+++ src/components/cells/certificate-status.cell.ts
@@ -1,36 +1,32 @@
 import { Component } from '@angular/core';
 import { CellRendererContext, CommonModule } from '@c8y/ngx-components';
-import {
-  DevityCertificateStatus,
-  DevityDeviceCertificate,
-} from '~models/rest-reponse.model';
+import { DevityCertificateStatus } from '~models/rest-reponse.model';
 
 @Component({
   standalone: true,
-  template: '<span [attr.class]="class"><i [c8yIcon]="icon"></i> {{ status | translate }}</span>',
+  template:
+    '<span [attr.class]="class"><i [c8yIcon]="icon"></i> {{ context.item.status | translate }}</span>',
   imports: [CommonModule],
 })
 export class CertificateStatusCellRendererComponent {
-  status = DevityCertificateStatus.UNKNOWN;
   icon = 'help';
   class = '';
 
   constructor(public context: CellRendererContext) {
-    const certificate: DevityDeviceCertificate = context.item;
-    const now = new Date().getTime();
-
-    if (!!certificate.revokedAt) {
-      this.status = DevityCertificateStatus.REVOKED;
-      this.icon = 'exclamation-circle';
-      this.class = 'text-muted';
-    } else if (certificate.expiredAt <= now) {
-      this.status = DevityCertificateStatus.EXPIRED;
-      this.icon = 'cross-circle';
-      this.class = 'text-danger';
-    } else {
-      this.status = DevityCertificateStatus.VALID;
-      this.icon = 'ok';
-      this.class = 'text-success';
+    switch (context.item.status) {
+      case DevityCertificateStatus.VALID:
+        this.icon = 'ok';
+        this.class = 'text-success';
+        break;
+      case DevityCertificateStatus.REVOKED:
+        this.icon = 'exclamation-circle';
+        this.class = 'text-muted';
+        break;
+      case DevityCertificateStatus.EXPIRED:
+        this.icon = 'cross-circle';
+        this.class = 'text-danger';
+        break;
+      default:
     }
   }
 }
